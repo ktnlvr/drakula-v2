08975ea1d9b9b995e3a45ebb6186a5a24d73ea4a
Unai San Segundo <unais@metropolia.fi>
2024-11-18 14:39:40 +0200

    Base scene for the frontend

--- src/frontend/src/Scene.svelte
+++ src/frontend/src/Scene.svelte
@@ -1,14 +1,23 @@
 <script>
-    import { T, useTask } from "@threlte/core";
-
-    let rotation = 0;
-    useTask((dt) => {
-        const SPEED = 4;
-        rotation += dt * SPEED;
-    });
+  import { T } from "@threlte/core";
+  import { Grid } from "@threlte/extras";
+  import CameraControls from "./components/CameraControls.svelte";
+  import Lights from "./components/Lights.svelte";
 </script>
 
-<T.Mesh rotation.y={rotation} rotation.x={rotation}>
-    <T.BoxGeometry />
-    <T.MeshBasicMaterial />
-</T.Mesh>
+<T.PerspectiveCamera
+  makeDefault
+  position={[100, 1, 1]}
+  fov={90}
+  oncreate={(ref) => {
+    ref.lookAt(0, 0, 0);
+  }}
+>
+  <CameraControls
+    oncreate={(ref) => {
+      globalThis.$cameraControls = ref;
+    }}
+  />
+</T.PerspectiveCamera>
+<Lights />
+<Grid sectionThickness={0} infiniteGrid cellColor="#dddddd" cellSize={2} />
---
+++ src/frontend/src/components/Lights.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { T } from "@threlte/core";
+</script>
+
+<T.DirectionalLight position={[3, 3, 3]} intensity={1.5} />
+<T.AmbientLight intensity={0.1} />
