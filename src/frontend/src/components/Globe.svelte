8d63bee15abcee00b62fcb6121d8d2199d9e6455
Unai San Segundo <unais@metropolia.fi>
2024-11-19 14:20:53 +0200

    Group points and earth

--- src/frontend/src/components/Globe.svelte
+++ src/frontend/src/components/Globe.svelte
@@ -2,11 +2,11 @@
   import { T, useTask } from "@threlte/core";
   import { useTexture } from "@threlte/extras";
 
-  let earthRef;
+  let globeGroupRef;
   let pointCoords;
   const dayTexture = useTexture("/EarthColor.png");
   const normalTexture = useTexture("/EarthNormal.png");
-  const EARTH_SPEED = 0;
+  const GLOBE_SPEED = 0.0005;
 
   function PosFromLatLon(phi, theta) {
     let lat = (90 - phi) * (Math.PI / 180);
@@ -19,28 +19,30 @@
   }
   pointCoords = PosFromLatLon(40.41615654093708, -3.6961276006060904);
   useTask(() => {
-    if (earthRef) {
-      earthRef.rotation.y += EARTH_SPEED;
+    if (globeGroupRef) {
+      globeGroupRef.rotation.y += GLOBE_SPEED;
     }
   });
 </script>
 
-{#await dayTexture then day}
-  {#await normalTexture then normal}
-    <T.Mesh position={[0, 0, 0]} bind:ref={earthRef}>
-      <T.SphereGeometry args={[30, 128, 64]} />
-      <T.MeshStandardMaterial
-        map={day}
-        normalMap={normal}
-        normalScale={0.5}
-        roughness={0.8}
-        metalness={0.1}
-        envMapIntensity={1}
-        aoMapIntensity={1}
-      />
-    </T.Mesh>
-  {/await}{/await}
-<T.Mesh position={[pointCoords.x, pointCoords.y, pointCoords.z]}>
-  <T.SphereGeometry args={[0.3, 128, 64]} />
-  <T.MeshStandardMaterial color={"#ff0000"} />
-</T.Mesh>
+<T.Group bind:ref={globeGroupRef}>
+  {#await dayTexture then day}
+    {#await normalTexture then normal}
+      <T.Mesh position={[0, 0, 0]}>
+        <T.SphereGeometry args={[30, 128, 64]} />
+        <T.MeshStandardMaterial
+          map={day}
+          normalMap={normal}
+          normalScale={0.5}
+          roughness={0.8}
+          metalness={0.1}
+          envMapIntensity={1}
+          aoMapIntensity={1}
+        />
+      </T.Mesh>
+    {/await}{/await}
+  <T.Mesh position={[pointCoords.x, pointCoords.y, pointCoords.z]}>
+    <T.SphereGeometry args={[0.3, 128, 64]} />
+    <T.MeshStandardMaterial color={"#ff0000"} />
+  </T.Mesh>
+</T.Group>
