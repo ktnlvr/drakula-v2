a81865f5bf0920ed60d7a1c4716ff4006df51580
Unai San Segundo <unais@metropolia.fi>
2024-11-19 14:09:49 +0200

    Get position for airport from latitude and longitude

--- src/frontend/src/components/Globe.svelte
+++ src/frontend/src/components/Globe.svelte
@@ -3,10 +3,21 @@
   import { useTexture } from "@threlte/extras";
 
   let earthRef;
+  let pointCoords;
   const dayTexture = useTexture("/EarthColor.png");
   const normalTexture = useTexture("/EarthNormal.png");
   const EARTH_SPEED = 0;
 
+  function PosFromLatLon(phi, theta) {
+    let lat = (90 - phi) * (Math.PI / 180);
+    let lon = (theta + 180) * (Math.PI / 180);
+    let coords = {};
+    coords.x = 30 * -(Math.sin(lat) * Math.cos(lon));
+    coords.z = 30 * Math.sin(lat) * Math.sin(lon);
+    coords.y = 30 * Math.cos(lat);
+    return coords;
+  }
+  pointCoords = PosFromLatLon(40.41615654093708, -3.6961276006060904);
   useTask(() => {
     if (earthRef) {
       earthRef.rotation.y += EARTH_SPEED;
@@ -29,3 +40,7 @@
       />
     </T.Mesh>
   {/await}{/await}
+<T.Mesh position={[pointCoords.x, pointCoords.y, pointCoords.z]}>
+  <T.SphereGeometry args={[0.3, 128, 64]} />
+  <T.MeshStandardMaterial color={"#ff0000"} />
+</T.Mesh>
