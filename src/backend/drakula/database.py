cc40e8c350639dd87779771a4d449bc2ca361285
Lion504 <lehtonen6677@gmail.com>
2024-11-20 17:35:52 +0200

    post+get data from mariadb

--- src/backend/drakula/database.py
+++ src/backend/drakula/database.py
@@ -1,3 +1,4 @@
+import uuid
 from os import getenv
 from typing import Optional
 from hashlib import md5
@@ -29,11 +30,57 @@ class Database:
                 FROM airport ORDER BY RAND({seed}) LIMIT {amount or DEFAULT_AIRPORT_AMOUT}"""
         )
         return list(map(lambda args: Airport(**args), cursor.fetchall()))
+    
+    def set_save(self, id, json):
+        if not self.connection:
+            print("Database connection error!")
+            return None
+        cursor = self.connection.cursor()
+        
+        cursor.execute(
+            f"""INSERT INTO games (id) VALUE (%s, ?, %s)""",
+            (id, json)
+        )
+        print(f"Insert new player complete!")
 
+    
+    def get_save(self,game_id: str, amount: int = DEFAULT_AIRPORT_AMOUT) -> list[dict]:
+        if not self.connection:
+            print("Database connection error!")
+            return None
+        cursor = self.connection.cursor(dictionary=True)
+        cursor.execute(
+            "SELECT * FROM games WHERE id = %s",
+            (game_id,)
+        )
+
+        return cursor.fetchall()        
 
 def make_db() -> Database:
     host = getenv("DRAKULA_V2_MARIADB_HOST") or "127.0.0.1"
     port = int(getenv("DRAKULA_V2_MARIADB_PORT") or "3306")
     password = getenv("DRAKULA_V2_MARIADB_PASSWORD")
     user = getenv("DRAKULA_V2_MARIADB_USER")
-    return Database(user=user, password=password, host=host, port=port)
+    db = Database(user=user, password=password, host=host, port=port)
+
+    if not db.connection:
+        print("Database connection error!")
+    else:
+        init_query = read_sql_query('query.sql')
+        cursor = db.connection.cursor()
+        cursor.execute(init_query)
+    
+    return db
+
+
+def read_sql_query(filename: str)-> str:
+    with open(filename, 'r') as file:
+        return file.read()
+
+
+if __name__ == "__main__":
+    db = make_db()
+    if db:
+        airports = db.get_airports(seed="example_seed", amount=10)
+        for airport in airports:
+            print(airport)
