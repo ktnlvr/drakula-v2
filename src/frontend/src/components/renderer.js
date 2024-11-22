bd6e635b64ce2b39b74110251ee92e670f7ea5a2
Unai San Segundo <unais@metropolia.fi>
2024-11-22 22:51:49 +0200

    Moved missing renderer setup and loop to the renderer component

--- src/frontend/src/components/renderer.js
+++ src/frontend/src/components/renderer.js
@@ -1,6 +1,16 @@
 import * as THREE from "three";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
+import { OutlinePass } from "three/examples/jsm/postprocessing/OutlinePass.js";
+import { OutputPass } from "three/addons/postprocessing/OutputPass.js";
+import { InteractionManager } from "three.interactive";
+import Stats from "stats-gl";
 
-export function createRenderer() {
+let stats;
+let interactionManager;
+let composer;
+const clock = new THREE.Clock();
+export function createRenderer(scene, camera) {
   const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.outputColorSpace = THREE.SRGBColorSpace;
   renderer.shadowMap.enabled = true;
@@ -10,5 +20,48 @@ export function createRenderer() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-  return renderer;
+  composer = new EffectComposer(renderer);
+  const renderPass = new RenderPass(scene, camera);
+  composer.addPass(renderPass);
+
+  const outlinePass = new OutlinePass(
+    new THREE.Vector2(window.innerWidth, window.innerHeight),
+    scene,
+    camera
+  );
+  outlinePass.edgeStrength = 3;
+  outlinePass.edgeGlow = 2;
+  outlinePass.edgeThickness = 1;
+  outlinePass.pulsePeriod = 1.2;
+  outlinePass.visibleEdgeColor.set("#ffffff");
+
+  composer.addPass(outlinePass);
+  const outputPass = new OutputPass();
+  composer.addPass(outputPass);
+
+  stats = new Stats({ trackGPU: true });
+  stats.init(renderer);
+  interactionManager = new InteractionManager(
+    renderer,
+    camera,
+    renderer.domElement
+  );
+  document.body.appendChild(renderer.domElement);
+  document.body.appendChild(stats.dom);
+
+  return {
+    renderer: renderer,
+    outlinePass: outlinePass,
+    composer: composer,
+    interactionManager: interactionManager,
+  };
+}
+export function render(cameraControls, spotlightHelper) {
+  cameraControls.update(clock.getDelta());
+  interactionManager.update();
+  spotlightHelper.update();
+
+  composer.render();
+  stats.update();
+  requestAnimationFrame(() => render(cameraControls, spotlightHelper));
 }
--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,74 +1,31 @@
 import * as THREE from "three";
-import Stats from "stats-gl";
 import { createCamera, setControls } from "./components/camera";
 import { createGlobe, createTable } from "./components/assets";
 import { setupLights } from "./components/lights";
-import { createRenderer } from "./components/renderer";
-import { InteractionManager } from "three.interactive";
-import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
-import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
-import { OutlinePass } from "three/examples/jsm/postprocessing/OutlinePass.js";
-import { OutputPass } from "three/addons/postprocessing/OutputPass.js";
-import CameraControls from "camera-controls";
+import { createRenderer, render } from "./components/renderer";
 import { setupGui } from "./components/gui";
+import CameraControls from "camera-controls";
 
-let stats;
-const clock = new THREE.Clock();
 const scene = new THREE.Scene();
 const camera = createCamera();
-const renderer = createRenderer();
-
-const composer = new EffectComposer(renderer);
-const renderPass = new RenderPass(scene, camera);
-composer.addPass(renderPass);
-
-const outlinePass = new OutlinePass(
-  new THREE.Vector2(window.innerWidth, window.innerHeight),
+const { renderer, outlinePass, composer, interactionManager } = createRenderer(
   scene,
   camera
-);
-outlinePass.edgeStrength = 3;
-outlinePass.edgeGlow = 2;
-outlinePass.edgeThickness = 1;
-outlinePass.pulsePeriod = 1.2;
-outlinePass.visibleEdgeColor.set("#ffffff");
-
-composer.addPass(outlinePass);
-const outputPass = new OutputPass();
-composer.addPass(outputPass);
-
-document.body.appendChild(renderer.domElement);
-const interactionManager = new InteractionManager(
-  renderer,
-  camera,
-  renderer.domElement
 );
 
 scene.add(createGlobe(interactionManager, outlinePass));
 scene.add(createTable());
-const spotlight = setupLights(scene);
+
+const { ambientLight, spotlight } = setupLights(scene);
 const spotlightHelper = new THREE.SpotLightHelper(spotlight);
+setupGui(ambientLight, spotlight, spotlightHelper, renderer, scene);
 scene.add(spotlightHelper);
-setupGui(spotlight, renderer, scene);
+
+CameraControls.install({ THREE });
 const cameraControls = new CameraControls(camera, renderer.domElement);
 setControls(cameraControls);
 
-stats = new Stats({ trackGPU: true });
-stats.init(renderer);
-document.body.appendChild(stats.dom);
-
-function render() {
-  const delta = clock.getDelta();
-  cameraControls.update(delta);
-  interactionManager.update();
-  spotlightHelper.update();
-
-  composer.render();
-  stats.update();
-  requestAnimationFrame(render);
-}
-
-render();
+render(cameraControls, spotlightHelper);
 
 window.addEventListener(
   "resize",
