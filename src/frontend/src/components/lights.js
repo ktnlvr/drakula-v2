380142e775c03b3c4be1bf07be27cee583e725a2
Unai San Segundo <unais@metropolia.fi>
2024-11-28 18:24:16 +0200

    Tweaked light values

--- src/frontend/src/components/lights.js
+++ src/frontend/src/components/lights.js
@@ -1,10 +1,10 @@
 import * as THREE from "three";
 
 export function setupLights(scene) {
-  const ambientLight = new THREE.AmbientLight("#fdd1a5", 0.7);
+  const ambientLight = new THREE.AmbientLight("#fdd1a5", 0.9);
   scene.add(ambientLight);
 
-  const spotlight = new THREE.SpotLight("#ff9329", 600, 300, 0.45, 1, 0.75);
+  const spotlight = new THREE.SpotLight("#ff9329", 700, 300, 0.45, 1, 0.75);
   spotlight.position.set(70, 190, 0);
   spotlight.lookAt(new THREE.Vector3(0, 0, 0));
   spotlight.castShadow = true;
