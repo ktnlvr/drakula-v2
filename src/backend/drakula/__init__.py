fa737ef1a92cd4ce9f556bf7677fdc4c1952f587
Lion504 <lehtonen6677@gmail.com>
2024-12-03 00:13:36 +0200

    Fix Mariadb and FastAPI connection and communication problems.

--- src/backend/drakula/__init__.py
+++ src/backend/drakula/__init__.py
@@ -1,34 +1,31 @@
 from dotenv import load_dotenv
 from os import getenv
-from typing import Annotated, Optional,Dict
-from functools import lru_cache
-
-from fastapi import FastAPI, Depends,HTTPException
+import os
+from typing import Annotated, Optional, Dict
+#from functools import lru_cache
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.staticfiles import StaticFiles
-
 
 from .models import AirportsResponse
 from .database import Database, make_db, DEFAULT_AIRPORT_AMOUT
 
+dotenv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../backend/drakula/example.env'))
 
-
-load_dotenv()
+load_dotenv(dotenv_path=dotenv_path)
 
 if not (VITE_DIR := getenv("VITE_BUILD_PATH")):
 
     from os.path import abspath, curdir as cwd
-    print(f"Loaded VITE_BUILD_PATH: {VITE_DIR}")
-
     print(
         f"Environmental variable VITE_BUILD_PATH not defined, assuming `{abspath(cwd)}`"
     )
+    VITE_DIR = abspath(cwd)
 
 app = FastAPI()
-
+#print(f"VITE_DIR: {VITE_DIR}")
 static = StaticFiles(directory=VITE_DIR)
 
 DATABASE = None
-
 
 def db() -> Database:
     global DATABASE
@@ -36,12 +33,11 @@ def db() -> Database:
         DATABASE = make_db()
     return DATABASE
 
-
 @app.get("/airports")
 def airports(
     db: Annotated[Database, Depends(db)],
     seed: Optional[str] = None,
-    amount=DEFAULT_AIRPORT_AMOUT,
+    amount: int = DEFAULT_AIRPORT_AMOUT,
 ) -> AirportsResponse:
     airports = db.get_airports(seed=seed, amount=amount)
     return AirportsResponse.from_airports(airports)
--- src/backend/drakula/database.py
+++ src/backend/drakula/database.py
@@ -1,34 +1,43 @@
 
 import uuid
+from dotenv import load_dotenv
 from os import getenv
+import os
 from typing import Optional
 from hashlib import md5
 
-from mysql.connector import connect
+from mysql.connector import connect, Error
 
 from .models import Airport
 
 DEFAULT_AIRPORT_AMOUT = 15
 
+load_dotenv()
 
 class Database:
     def __init__(self, user, password, host, port):
-        self.connection = connect(
-            host=host,
-            port=port,
-            user=user,
-            password=password,
-            database="flight_game",
-            autocommit=True,
-        )
+        try:
+            self.connection = connect(
+                host=host,
+                port=port,
+                user=user,
+                password=password,
+                database="flight_game",
+                autocommit=True,
+                auth_plugin="mysql_native_password"  
+            )
+        except Error as e:
+            print(f"Error: {e}")
+            self.connection = None
 
     def get_airports(self, seed: Optional[str] = None, amount: int = DEFAULT_AIRPORT_AMOUT) -> list[Airport]:
         cursor = self.connection.cursor(dictionary=True)
         seed = (seed or '') and (int.from_bytes(md5(seed.encode()).digest()) % 2**16)
         cursor.execute(
-            f"""SELECT
+             """SELECT
                 name, latitude_deg, longitude_deg, iso_country
-                FROM airport ORDER BY RAND({seed}) LIMIT {amount or DEFAULT_AIRPORT_AMOUT}"""
+                FROM airports ORDER BY RAND(%s) LIMIT %s""",
+            (seed, amount)
         )
         return list(map(lambda args: Airport(**args), cursor.fetchall()))
     
@@ -38,11 +47,14 @@ class Database:
             return None
         cursor = self.connection.cursor()
         
-        cursor.execute(
-            f"""INSERT INTO games (id) VALUE (%s, ?, %s)""",
-            (id, json)
-        )
-        print(f"Insert new player complete!")
+        try:
+            cursor.execute(
+                """INSERT INTO games (id, json_data) VALUES (%s, %s)""",
+                (id, json)
+            )
+            print(f"Insert new player complete!")
+        except Error as e:
+            print(f"Error: {e}")
 
     
     def get_save(self,game_id: str, amount: int = DEFAULT_AIRPORT_AMOUT) -> list[dict]:
@@ -57,25 +69,32 @@ class Database:
 
         return cursor.fetchall()        
 
-def make_db() -> Database:
-    host = getenv("DRAKULA_V2_MARIADB_HOST") or "127.0.0.1"
-    port = int(getenv("DRAKULA_V2_MARIADB_PORT") or "3306")
-    password = getenv("DRAKULA_V2_MARIADB_PASSWORD")
-    user = getenv("DRAKULA_V2_MARIADB_USER")
-    db = Database(user=user, password=password, host=host, port=port)
+def make_db() -> Optional[Database]:
+    try:
+        db = Database (
+            host = getenv("DRAKULA_V2_MARIADB_HOST") or "127.0.0.1",
+            port = int(getenv("DRAKULA_V2_MARIADB_PORT") or "3306"),
+            password = getenv("DRAKULA_V2_MARIADB_PASSWORD"),
+            user = getenv("DRAKULA_V2_MARIADB_USER")
+        )
 
-    if not db.connection:
-        print("Database connection error!")
-    else:
-        init_query = read_sql_query('query.sql')
+        if not db.connection:
+            print("Database connection error!")
+            return None
+        full_path = 'query.sql'
+        init_query = read_sql_query(full_path)
         cursor = db.connection.cursor()
         cursor.execute(init_query)
+        return db
     
-    return db
-    
+    except Error as e:
+        print(f"Error: {e}") 
+        return None           
 
 def read_sql_query(filename: str)-> str:
-    with open(filename, 'r') as file:
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    full_path = os.path.abspath(os.path.join(base_dir, 'query.sql'))
+    with open(full_path, 'r') as file:
         return file.read()
 
 
